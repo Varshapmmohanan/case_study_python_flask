155cef5828facd546f13d89d059735d1ea77ce09
Varshapmmohanan <148255376+Varshapmmohanan@users.noreply.github.com>
2024-01-21 12:35:19 +0000

    2

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask,render_template,request
+import pickle
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+@app.route('/prediction', methods = ['GET','POST'])
+def predict():
+    if request.method == 'POST':
+        PL = request.form['PL']
+        PW = request.form['PW']
+        print(PL,PW)
+        pickled_model = pickle.load(open('model.pkl','rb'))
+        Classification = pickled_model.predict([[float(PL,PW)]])
+        print(Classification)
+    
+    return render_template('prediction.html')
+
+
+if __name__ == '__main__':
+    app.run()
